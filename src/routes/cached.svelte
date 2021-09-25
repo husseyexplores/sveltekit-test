727a4909d84c0f5bbefe66d66e1f32e1b1a63909
husseyexplores <husseyexplores@gmail.com>
2021-09-25 18:04:37 +0500

    4

---
+++ src/routes/cached.svelte
@@ -0,0 +1,36 @@
+<script context="module">
+  export const prerender = true;
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+   export async function load({ page, fetch, session, stuff }) {
+		const url = `https://us-central1-shop-husseyfns.cloudfunctions.net/echo`;
+		const data = await fetch(url).then(r => r.json()).catch(e => null)
+    console.log('\n------')
+    console.log('Building cached')
+    console.log('------\n')
+
+		if (data) {
+			return {
+				props: {
+          content: data._reqUid,
+          maxage: 'puclic'
+				}
+			};
+		}
+
+		return {
+			status: 400,
+			error: new Error(`Could not load ${url}`)
+		};
+	}
+</script>
+
+<script>
+  export let content
+</script>
+
+<h1>About</h1>
+<div>
+  {content}
+</div>
